dd3e826603cac48fb23bb3c54df80eb1881edae4
mozeroman <mozeroman@gmail.com>
2014-05-30 08:31:07 +0800

    New Post model added

--- lessonkill/chapter/models.py
+++ lessonkill/chapter/models.py
@@ -1,5 +1,6 @@
 #coding=utf-8
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 
@@ -11,12 +12,12 @@ class Posts(models.Model):
             )
 
     post_type = models.CharField('展出类型', max_length = 30, choices = POSTS_TYPE)
-    post_index = models.TextField('内容')
+    post_index = models.TextField('内容', )
     post_date = models.DateTimeField('发布时间', auto_now = True)
-    post_deadline = models.DateTimeField('截止日期')
+    post_deadline = models.DateTimeField('截止日期', )
 
     class Meta:
         ordering = ['-post_deadline']
 
-    def __unicode__():
+    def __unicode__(self):
         return u'%s %s' % (self.post_type, self.post_date)
